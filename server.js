3b765ef386f20ec7fc592e4a2adf757eea86a150
viktmv <viktmv@icloud.com>
2017-06-01 14:11:18 +0000

    Add support for the newest chatty-app features

--- server.js
+++ server.js
@@ -14,20 +14,55 @@ const server = express()
 // Create the WebSockets server
 const wss = new SocketServer({ server });
 
+// store all messages to send every newly conncted user
+const messages = []
+const colours = ['navy', 'fuchsia', 'lime', 'teal']
+
+const rndColor = () => Math.floor(Math.random() * 3)
+let counter = 0
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  counter++
+  let userColour = colours[rndColor()]
 
+  ws.send(JSON.stringify({colour: userColour }))
+
+  messages.forEach(message => {
+    ws.send(JSON.stringify({message, counter}))
+  })
+
+  wss.clients.forEach(c => c.send(JSON.stringify({counter})))
   ws.on('message', function incoming(data) {
-    let message = JSON.parse(data)
+    let {message} = JSON.parse(data)
+    let {type} = message
+
     message.id = uuidV4()
 
+    // if (!message.colour) message.colour = userColour
+
+    if (type === 'postNotification') {
+      message.type = 'incomingNotification'
+    }
+    else {
+      message.type = 'incomingMessage'
+    }
+
+    messages.push(message)
+
     wss.clients.forEach(function each(client) {
-       client.send(JSON.stringify(message));
+      client.send(JSON.stringify({message}))
     })
+    // }
   })
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('close', () => {
+    console.log('Client disconnected')
+    counter--
+    wss.clients.forEach(c => {
+        c.send(JSON.stringify({counter}))
+    })
+  })
 })
